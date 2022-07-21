c3fa2a62ce841080e1ab5f0bc0ad149cc7005bbf
Holly Haller <thehallers@icloud.com>
2022-07-20 19:17:04 -0700

    main folder

---
+++ Main/controllers/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+const { application } = require('express');
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
